7c3ae0d4c65a7216f472a00497a37924c4842183
035495022224 <035495022224@RAMWSEDS03.tre-am.gov.br>
2019-10-14 16:21:55 -0400

    alteracaoDAO

---
+++ src/br/jus/tream/DAO/ProdutoDAO.java
@@ -0,0 +1,21 @@
+package br.jus.tream.DAO;
+
+import java.util.List;
+
+import br.jus.tream.dominio.Produto;
+
+public interface ProdutoDAO {
+	
+	public List<Produto> listar() throws Exception;
+	
+	public List<Produto> listar(String vdesc) throws Exception;
+	
+	public Produto getBean(int id) throws Exception;
+	
+	public int adicionar (Produto produto) throws Exception;
+	
+	public int atualizar (Produto produto) throws Exception;
+	
+	public int remover (Produto produto) throws Exception;
+	
+} 
